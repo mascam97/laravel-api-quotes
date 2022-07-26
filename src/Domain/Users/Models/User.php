b2b78c138896222d3aebc93dd4120cd30d216c48
Martín Campos <martin.stepwolf@gmail.com>
2022-07-26 17:58:41 -0500

    PHPStan to level 3

--- src/Domain/Users/Models/User.php
+++ src/Domain/Users/Models/User.php
@@ -33,22 +33,12 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasApiTokens, CanRate;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password',
         'remember_token',
--- src/Support/Providers/AuthServiceProvider.php
+++ src/Support/Providers/AuthServiceProvider.php
@@ -8,11 +8,6 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
     protected $policies = [
         Quote::class => QuotePolicy::class,
     ];
